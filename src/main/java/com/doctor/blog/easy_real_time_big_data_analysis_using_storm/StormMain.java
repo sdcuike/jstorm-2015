2d8680f8d21227d9178a8b0f76caa294e82c863b
sdcuike <sdcuike@users.noreply.github.com>
2015-05-08 17:26:21 +0800

    StormMain

---
+++ src/main/java/com/doctor/blog/easy_real_time_big_data_analysis_using_storm/StormMain.java
@@ -0,0 +1,16 @@
+package com.doctor.blog.easy_real_time_big_data_analysis_using_storm;
+
+/**
+ * @see http://www.csdn.net/article/2012-12-24/2813117-storm-realtime-big-data-analysis
+ * 
+ * @author doctor
+ *
+ * @time 2015年5月8日
+ */
+public class StormMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
