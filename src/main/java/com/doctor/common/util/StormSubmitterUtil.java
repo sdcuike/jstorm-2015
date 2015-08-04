0d18669b7fb4afb64ae1b41b669810aa5504fae9
sdcuike <sdcuike@users.noreply.github.com>
2015-08-04 17:43:48 +0800

    Topology 的Config从配置文件中读取

--- src/main/java/com/doctor/common/util/StormSubmitterUtil.java
+++ src/main/java/com/doctor/common/util/StormSubmitterUtil.java
@@ -17,14 +17,43 @@ import backtype.storm.generated.StormTopology;
  * Topology 提交工具类
  * 
  * @see 参考：https://github.com/apache/storm/blob/master/examples/storm-starter/src/jvm/storm/starter/util/StormRunner.java
- * 
+ *      https://github.com/alibaba/jstorm/blob/master/example/sequence-split-merge/src/main/java/com/alipay/dw/jstorm/example/sequence/SequenceTopology.java
  * @author doctor
  *
  * @time 2015年5月9日 下午10:36:59
  */
 public final class StormSubmitterUtil {
 
-	public static void submitTopologyLocally(StormTopology topology, String topologyName, Config conf, int runtimeInMinutes) throws InterruptedException {
+	/**
+	 * Topology 的Config从配置文件中读取
+	 * 
+	 * @param topology
+	 * @param propFile
+	 * @throws InterruptedException
+	 * @throws AlreadyAliveException
+	 * @throws InvalidTopologyException
+	 */
+	public static void submitTopology(final StormTopology topology, final String propFile) throws InterruptedException, AlreadyAliveException, InvalidTopologyException {
+		Config config = LoadConfigFromPropertyFile(propFile);
+
+		String topologyName = (String) config.get(Config.TOPOLOGY_NAME);
+		if (isLocalCluster(config)) {
+			submitTopologyLocally(topology, topologyName, config, 20);
+		} else {
+			submitTopologyRemotely(topology, topologyName, config);
+		}
+	}
+
+	public static void submitTopology(final StormTopology topology, final Config config) throws InterruptedException, AlreadyAliveException, InvalidTopologyException {
+		String topologyName = (String) config.get(Config.TOPOLOGY_NAME);
+		if (isLocalCluster(config)) {
+			submitTopologyLocally(topology, topologyName, config, 20);
+		} else {
+			submitTopologyRemotely(topology, topologyName, config);
+		}
+	}
+
+	public static void submitTopologyLocally(final StormTopology topology, final String topologyName, final Config conf, final int runtimeInMinutes) throws InterruptedException {
 		LocalCluster localCluster = new LocalCluster();
 		localCluster.submitTopology(topologyName, conf, topology);
 		TimeUnit.MINUTES.sleep(runtimeInMinutes);
@@ -32,7 +61,7 @@ public final class StormSubmitterUtil {
 		localCluster.shutdown();
 	}
 
-	public static void submitTopologyRemotely(StormTopology topology, String topologyName, Config conf) throws AlreadyAliveException, InvalidTopologyException {
+	public static void submitTopologyRemotely(final StormTopology topology, final String topologyName, final Config conf) throws AlreadyAliveException, InvalidTopologyException {
 		StormSubmitter.submitTopology(topologyName, conf, topology);
 	}
 
@@ -58,4 +87,13 @@ public final class StormSubmitterUtil {
 
 		return config;
 	}
+
+	public static boolean isLocalCluster(final Config config) {
+		String mode = (String) config.get(Config.STORM_CLUSTER_MODE);
+		if ("local".equals(mode)) {
+			return true;
+		}
+		return false;
+	}
+
 }
